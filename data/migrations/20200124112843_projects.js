21ae66c35fc7fbbc75712c1dd5f4ad18939961f7
Aaron <aarongillies88@gmail.com>
2020-01-24 11:57:40 -0500

    migrations built out

--- data/migrations/20200124112843_projects.js
+++ data/migrations/20200124112843_projects.js
@@ -1,8 +1,40 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('projects', table => {
+        table.increments()
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('completed').notNullable()
+    })
+    .createTable('resources', table => {
+        table.increments()
+        table.string('resource_name').notNullable().unique();
+        table.string('resource_description')
+    })
+    .createTable('tasks', table => {
+        table.increments()
+        table.integer('project_id')
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        table.string('task_description').notNullable()
+        table.string('notes')
+        table.boolean().notNullable().defaultTo('false')
+    })
+    .createTable('project_resources', table => {
+        table.integer('project_id')
+        .references('id')
+        .inTable('projects')
+        table.integer('resource_id')
+        .references('id')
+        .inTable('resources')
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('project_resources')
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('resources')
+  .dropTableIfExists('projects')
 };
